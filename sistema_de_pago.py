e3d08e81076106920c292b5aa354bbdb452d65bd
kenny <kennypinto60@gmail.com>
2026-03-26 00:36:49 -0400

    Add files via upload

---
+++ sistema_de_pago.py
@@ -0,0 +1,34 @@
+
+num2=1
+num3=2
+num4=3
+num5=4
+num6=5
+#creada por kenny pinto
+#el proyecto es de pagos dependiendo su rango
+print("**********")
+print("bienvenido")
+print("**********")
+print("retira tu pago segun tu rango")
+print("1.jefe de proyecto👨‍💼")
+print("2.administrador🏢")
+print("3.programador💻")
+print("4.secretario✏️")
+print("5.conserge🧹 Escoba Emoji")
+#aqui se elige el numero del rango
+res=int(input("en que posicion de trabajo estas "))
+if res==num2:
+    print("eres el jefe de proyecto tu pago es de 400dolares💵 por esta semana")
+elif res==num3:
+    print("eres el administrador tu pago es de 350 dolares💵 por esta semana")
+elif res==num4:
+    print("eres el programador tu pago es de 350dolares💵 por esta semana")
+elif res==num5:
+    print("eres el secretario tu pago es de 200dolares💵 por esta semana")
+elif res==num6:
+    print("eres el conserge tu pago es de 100dolares💵 por esta semana")
+else:
+    print("error")
+
+
+
